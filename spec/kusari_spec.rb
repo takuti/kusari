3655c768e679384cedcfb296da7d87744764d023
takuti <k.takuti@gmail.com>
2015-12-15 02:05:19 +0900

    #1: update spec

--- spec/kusari_spec.rb
+++ spec/kusari_spec.rb
@@ -5,7 +5,14 @@ describe Kusari do
     expect(Kusari::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  let(:generator) { Kusari::Generator.new }
+  before do
+    generator.add_string("ネロとパトラッシュは、この世で二人きりでした。")
+    generator.add_string("彼らは、実の兄弟よりも仲のよい大の親友でした。")
+    generator.add_string("ネロは、アルデンネ生まれの少年でした。")
+  end
+
+  it 'generates a random sentence' do
+    expect(generator.generate(140)).to be_an_instance_of(String)
   end
 end
